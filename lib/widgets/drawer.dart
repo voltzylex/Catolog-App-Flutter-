2b0fcfd17aeccfa01dc5e4cc17d441ee881a13d0
volteeee <baymax732001@gmail.com>
2022-04-06 21:35:28 +0530

    day 9 completed

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  // const MyDrawer({ Key? key }) : super(key: key);
+  final imageurl =
+      'https://media.images.yourquote.in/post/large/0/0/10/396/W8NX1161.jpg';
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple.shade600,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              // padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                // margin: EdgeInsets.zero,
+                // decoration: BoxDecoration(color: Colors.redAccent),//picture background color
+                accountName: Text("Sushil Kumar"),
+                accountEmail: Text("baymax732001@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageurl),
+                ) /*Image.network(imageurl)*/,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.phone,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Phone",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Mail Me",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
