181477750f89732390f223af898688afc9d2e388
volteeee <baymax732001@gmail.com>
2022-04-10 19:51:45 +0530

    Day 10 changes completed

---
+++ lib/Models/catalog.dart
@@ -0,0 +1,19 @@
+class Items {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Items({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [Items(
+  id: "Voltee001",
+  name: "iPhone 12 Pro",
+  desc: "Apple iPhone 12th",
+  price: 999,
+  color: "#33505a",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
+)];
