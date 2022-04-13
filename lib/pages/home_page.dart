db59c78209fc7e331303cf05e7572479c0a32296
volteeee <baymax732001@gmail.com>
2022-04-13 20:29:36 +0530

    Day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,34 @@
-// import 'dart:ui';
 // Day 11 we learn about context, and constraints
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/Models/catalog.dart';
-
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Codepur";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(5, (index) => CatalogModel.item[0]);
